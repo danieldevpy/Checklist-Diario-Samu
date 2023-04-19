f42d215157c247347b68bb78722cc25a594b15da
Daniel <danielfernandes202@gmail.com>
2023-04-19 10:50:33 -0300

    save alterações banco

--- unity/admin.py
+++ unity/admin.py
@@ -5,7 +5,7 @@ from .models import User
 
 # Register your models here.
 class AdminUser(admin.ModelAdmin):
-    list_display = ('username', 'unity', 'usa', 'usb')
+    list_display = ('username', 'unity', 'usa', 'usb', 'is_staff', 'is_superuser')
     list_filter = ('unity__name',)
     list_editable = ('unity', 'usa', 'usb')
 
